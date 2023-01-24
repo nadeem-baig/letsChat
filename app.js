d87abbdcfc620325e44417ed77b43b1aa1468522
nadeem-baig <nadeem31baig@gmail.com>
2023-01-24 19:44:29 +0530

    Updated port

--- app.js
+++ app.js
@@ -2,74 +2,76 @@
 // Let's Chat
 //
 
-'use strict';
+"use strict";
 
-process.title = 'V-Safe';
+process.title = "V-Safe";
 
-require('colors');
+require("colors");
 
-var _ = require('lodash'),
-    path = require('path'),
-    fs = require('fs'),
-    express = require('express.oi'),
-    i18n = require('i18n'),
-    bodyParser = require('body-parser'),
-    cookieParser = require('cookie-parser'),
-    compression = require('compression'),
-    helmet = require('helmet'),
-    http = require('http'),
-    nunjucks = require('nunjucks'),
-    mongoose = require('mongoose'),
-    connectMongo = require('connect-mongo/es5'),
-    all = require('require-tree'),
-    psjon = require('./package.json'),
-    settings = require('./app/config'),
-    auth = require('./app/auth/index'),
-    core = require('./app/core/index');
-    mongoose.Promise = global.Promise;
-    var cors = require('cors')
+var _ = require("lodash"),
+    path = require("path"),
+    fs = require("fs"),
+    express = require("express.oi"),
+    i18n = require("i18n"),
+    bodyParser = require("body-parser"),
+    cookieParser = require("cookie-parser"),
+    compression = require("compression"),
+    helmet = require("helmet"),
+    http = require("http"),
+    nunjucks = require("nunjucks"),
+    mongoose = require("mongoose"),
+    connectMongo = require("connect-mongo/es5"),
+    all = require("require-tree"),
+    psjon = require("./package.json"),
+    settings = require("./app/config"),
+    auth = require("./app/auth/index"),
+    core = require("./app/core/index");
+mongoose.Promise = global.Promise;
+var cors = require("cors");
 
 var MongoStore = connectMongo(express.session),
     httpEnabled = settings.http && settings.http.enable,
     httpsEnabled = settings.https && settings.https.enable,
-    models = all(path.resolve('./app/models')),
-    middlewares = all(path.resolve('./app/middlewares')),
-    controllers = all(path.resolve('./app/controllers')),
+    models = all(path.resolve("./app/models")),
+    middlewares = all(path.resolve("./app/middlewares")),
+    controllers = all(path.resolve("./app/controllers")),
     app;
 
 //
 // express.oi Setup
 //
 if (httpsEnabled) {
-     app = express().https({
-        key: fs.readFileSync(settings.https.key),
-        cert: fs.readFileSync(settings.https.cert),
-        passphrase: settings.https.passphrase
-    }).io();
+    app = express()
+        .https({
+            key: fs.readFileSync(settings.https.key),
+            cert: fs.readFileSync(settings.https.cert),
+            passphrase: settings.https.passphrase,
+        })
+        .io();
 } else {
     app = express().http().io();
 }
 
-if (settings.env === 'production') {
-    app.set('env', settings.env);
-    app.set('json spaces', undefined);
-    app.enable('view cache');
+if (settings.env === "production") {
+    app.set("env", settings.env);
+    app.set("json spaces", undefined);
+    app.enable("view cache");
 }
 
 // Session
 var sessionStore = new MongoStore({
     url: settings.database.uri,
-    autoReconnect: true
+    autoReconnect: true,
 });
 
 // Session
 var session = {
-    key: 'connect.sid',
+    key: "connect.sid",
     secret: settings.secrets.cookie,
     store: sessionStore,
     cookie: { secure: httpsEnabled },
     resave: false,
-    saveUninitialized: true
+    saveUninitialized: true,
 };
 
 // Set compression before any routes
@@ -86,101 +88,110 @@ app.use(helmet.hidePoweredBy());
 app.use(helmet.ieNoOpen());
 app.use(helmet.noSniff());
 app.use(helmet.xssFilter());
-app.use(helmet.hsts({
-    maxAge: 3600000 ,
-    includeSubdomains: true,
-    force: httpsEnabled,
-    preload: true
-}));
-app.use(helmet.contentSecurityPolicy({
-    defaultSrc: ['\'none\''],
-    connectSrc: ['*'],
-    scriptSrc: ['\'self\'', '\'unsafe-eval\''],
-    styleSrc: ['\'self\'', 'fonts.googleapis.com', '\'unsafe-inline\''],
-    fontSrc: ['\'self\'', 'fonts.gstatic.com'],
-    mediaSrc: ['\'self\''],
-    objectSrc: ['\'self\''],
-    imgSrc: ['* data:']
-}));
+app.use(
+    helmet.hsts({
+        maxAge: 3600000,
+        includeSubdomains: true,
+        force: httpsEnabled,
+        preload: true,
+    })
+);
+app.use(
+    helmet.contentSecurityPolicy({
+        defaultSrc: ["'none'"],
+        connectSrc: ["*"],
+        scriptSrc: ["'self'", "'unsafe-eval'"],
+        styleSrc: ["'self'", "fonts.googleapis.com", "'unsafe-inline'"],
+        fontSrc: ["'self'", "fonts.gstatic.com"],
+        mediaSrc: ["'self'"],
+        objectSrc: ["'self'"],
+        imgSrc: ["* data:"],
+    })
+);
 
 var bundles = {};
-app.use(require('connect-assets')({
-    paths: [
-        'media/js',
-        'media/less'
-    ],
-    helperContext: bundles,
-    build: settings.env === 'production',
-    fingerprinting: settings.env === 'production',
-    servePath: 'media/dist'
-}));
+app.use(
+    require("connect-assets")({
+        paths: ["media/js", "media/less"],
+        helperContext: bundles,
+        build: settings.env === "production",
+        fingerprinting: settings.env === "production",
+        servePath: "media/dist",
+    })
+);
 
 // Public
-app.use('/media', express.static(__dirname + '/media', {
-    maxAge: '2000d'
-}));
+app.use(
+    "/media",
+    express.static(__dirname + "/media", {
+        maxAge: "2000d",
+    })
+);
 
-app.use(cors())
+app.use(cors());
 
 // Templates
-var nun = nunjucks.configure('templates', {
+var nun = nunjucks.configure("templates", {
     autoescape: true,
     express: app,
     tags: {
-        blockStart: '<%',
-        blockEnd: '%>',
-        variableStart: '<$',
-        variableEnd: '$>',
-        commentStart: '<#',
-        commentEnd: '#>'
-    }
+        blockStart: "<%",
+        blockEnd: "%>",
+        variableStart: "<$",
+        variableEnd: "$>",
+        commentStart: "<#",
+        commentEnd: "#>",
+    },
 });
 
 function wrapBundler(func) {
     // This method ensures all assets paths start with "./"
     // Making them relative, and not absolute
-    return function() {
-        return func.apply(func, arguments)
-                   .replace(/href="\//g, 'href="./')
-                   .replace(/src="\//g, 'src="./');
+    return function () {
+        return func
+            .apply(func, arguments)
+            .replace(/href="\//g, 'href="./')
+            .replace(/src="\//g, 'src="./');
     };
 }
 
-nun.addFilter('js', wrapBundler(bundles.js));
-nun.addFilter('css', wrapBundler(bundles.css));
-nun.addGlobal('text_search', false);
+nun.addFilter("js", wrapBundler(bundles.js));
+nun.addFilter("css", wrapBundler(bundles.css));
+nun.addGlobal("text_search", false);
 
 // i18n
 i18n.configure({
-    directory: path.resolve(__dirname, './locales'),
+    directory: path.resolve(__dirname, "./locales"),
     locales: settings.i18n.locales || settings.i18n.locale,
-    defaultLocale: settings.i18n.locale
+    defaultLocale: settings.i18n.locale,
 });
 app.use(i18n.init);
 
 // HTTP Middlewares
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
 
 // IE header
-app.use(function(req, res, next) {
-    res.setHeader('X-UA-Compatible', 'IE=Edge,chrome=1');
+app.use(function (req, res, next) {
+    res.setHeader("X-UA-Compatible", "IE=Edge,chrome=1");
     next();
 });
 
 //
 // Controllers
 //
-_.each(controllers, function(controller) {
+_.each(controllers, function (controller) {
     controller.apply({
         app: app,
         core: core,
         settings: settings,
         middlewares: middlewares,
         models: models,
-        controllers: controllers
+        controllers: controllers,
     });
 });
 
@@ -188,38 +199,41 @@ _.each(controllers, function(controller) {
 // Mongo
 //
 
-mongoose.connection.on('error', function (err) {
+mongoose.connection.on("error", function (err) {
     throw new Error(err);
 });
 
-mongoose.connection.on('disconnected', function() {
-    throw new Error('Could not connect to database');
+mongoose.connection.on("disconnected", function () {
+    throw new Error("Could not connect to database");
 });
 mongoose.connection.once("open", function () {
     console.log("Connected successfully");
-  });
+});
 //
 // Go Time
 //
 
 function startApp() {
-    var port = httpsEnabled && settings.https.port ||
-               httpEnabled && settings.http.port;
+    var port =
+        (httpsEnabled && settings.https.port) ||
+        (httpEnabled && settings.http.port);
 
-    var host = httpsEnabled && settings.https.host ||
-               httpEnabled && settings.http.host || '0.0.0.0';
-
-
+    var host =
+        (httpsEnabled && settings.https.host) ||
+        (httpEnabled && settings.http.host) ||
+        "0.0.0.0";
 
     if (httpsEnabled && httpEnabled) {
         // Create an HTTP -> HTTPS redirect server
         var redirectServer = express();
-        redirectServer.get('*', function(req, res) {
-            var urlPort = port === 80 ? '' : ':' + port;
-            res.redirect('https://' + req.hostname + urlPort + req.path);
+        redirectServer.get("*", function (req, res) {
+            var urlPort = port === 80 ? "" : ":" + port;
+            res.redirect("https://" + req.hostname + urlPort + req.path);
         });
-        http.createServer(redirectServer)
-            .listen(settings.http.port || 5000, host);
+        http.createServer(redirectServer).listen(
+            settings.http.port || 3000,
+            host
+        );
     }
 
     app.listen(port, host);
@@ -228,18 +242,18 @@ function startApp() {
     // XMPP
     //
     if (settings.xmpp.enable) {
-        var xmpp = require('./app/xmpp/index');
+        var xmpp = require("./app/xmpp/index");
         xmpp(core);
     }
 
-    var art = fs.readFileSync('./app/misc/art.txt', 'utf8');
-    console.log('\n' + art + '\n\n' + 'Release ' + psjon.version.yellow + '\n');
+    var art = fs.readFileSync("./app/misc/art.txt", "utf8");
+    console.log("\n" + art + "\n\n" + "Release " + psjon.version.yellow + "\n");
 }
 
 function checkForMongoTextSearch() {
     if (!mongoose.mongo || !mongoose.mongo.Admin) {
         // MongoDB API has changed, assume text search is enabled
-        nun.addGlobal('text_search', true);
+        nun.addGlobal("text_search", true);
         return;
     }
 
@@ -249,28 +263,32 @@ function checkForMongoTextSearch() {
             return;
         }
 
-        var version = info.version.split('.');
+        var version = info.version.split(".");
         if (version.length < 2) {
             return;
         }
 
-        if(version[0] < 2) {
+        if (version[0] < 2) {
             return;
         }
 
-        if(version[0] === '2' && version[1] < 6) {
+        if (version[0] === "2" && version[1] < 6) {
             return;
         }
 
-        nun.addGlobal('text_search', true);
+        nun.addGlobal("text_search", true);
     });
 }
 
-mongoose.connect(settings.database.uri,{useMongoClient:true}, function(err) {
-    if (err) {
-        throw err;
-    }
+mongoose.connect(
+    settings.database.uri,
+    { useMongoClient: true },
+    function (err) {
+        if (err) {
+            throw err;
+        }
 
-    checkForMongoTextSearch();
-    startApp();
-});
+        checkForMongoTextSearch();
+        startApp();
+    }
+);
